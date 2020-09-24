35aaee12c6d6eed32ac3ab705b7d7be294db6c43
Jacob Thomas <jacobmthomas9000@gmail.com>
2020-09-23 23:31:28 -0500

    V1

---
+++ Banking/Account.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banking
+{
+    class Account
+    {
+        private string NameOne = "name";
+        private int BalOne = 0;
+        public Account()
+        {
+            NameOne = "name";
+            BalOne = 0;
+
+        }
+        public Account(string NameOne, int BalOne)
+        {
+            this.NameOne = NameOne;
+            this.BalOne = BalOne;
+        }
+
+        //shows the account name and amount
+        public void show()
+        {
+            Console.WriteLine("User: " + NameOne + ", Balance: $" + BalOne);
+        }
+
+        //puts the deposit into the account and displays the account value
+        public void deposit(int DepoAmount)
+        {
+            //DepoAmount = 100;
+            BalOne = BalOne + DepoAmount;
+        }
+
+        //
+        public bool withdraw(int WithAmount)
+        {
+            if (BalOne - WithAmount >= 0)
+            {
+                BalOne = BalOne - WithAmount;
+                Console.WriteLine("Withdrawing money...");
+                Console.WriteLine("Success, new status: ");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Withdrawing money...");
+                Console.WriteLine("Insufficient funds, current status: ");
+                return false;
+            }
+        }
+        public bool transferA2(int amount, Account a2, Account a1)
+        {
+            Console.WriteLine("Transferring $" +amount+" from " + a2.NameOne + " to " + a1.NameOne + ".");
+            a2.deposit(amount);
+            a1.withdraw(amount);
+            return true;
+        }
+        public bool transferA1(int amount, Account a1, Account a2)
+        {
+            Console.WriteLine("Transferring $" + amount + " from " + a1.NameOne + " to " + a2.NameOne + ".");
+            a1.deposit(amount);
+            a2.withdraw(amount);
+            return true;
+        }
+    }
+}
+
---
+++ Banking/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Banking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start:");
+
+            //declaring the account
+            Account a1, a2;
+
+            a1 = new Account("Shane (A1)", 100);
+            a2 = new Account("Jacob (A2)", 200);
+            //using show method
+            a1.show();
+            //depositing 500 into account one (Shane)
+            a1.deposit(500);
+
+
+            //putting output in main because the only method that can output is show (look at assignment)
+
+
+            //deposit output
+            Console.WriteLine("Depositing money...");
+            Console.WriteLine("Success, new status:");
+            a1.show();
+
+
+
+
+            //withdraw output
+            a1.withdraw(50);
+ 
+            a1.show();
+
+            //transfering money from account one to account two
+            Console.WriteLine("Creating second account...");
+            a2.show();
+            
+            a1.transferA1(50, a1, a2);
+            a2.show();
+            a1.show();
+        }
+
+    }
+}
